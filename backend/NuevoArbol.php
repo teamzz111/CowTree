ce8357a846966dc05563a3bf80c5f67c2ee06544
Erika Infante <37392918+MonoAncestral@users.noreply.github.com>
2018-10-14 07:32:23 -0500

    tree under construction

--- backend/NuevoArbol.php
+++ backend/NuevoArbol.php
@@ -18,5 +18,29 @@ session_start();
         $rs = $con->query($query);
         if ($rs) {echo 'true';}
         else { cho 'false'; }
-    /*poner la wea de los niveles de los árboles*/
+
+
+        $Vaca = $_POST['vaca'];
+        $con = new mysqli($host, $user);
+        $con->query("SET NAMES 'utf8'");
+        if ($con->connect_error) {
+            echo 'false';
+        }
+        $pa=1;
+        while($pa=1)
+        {
+            $query1="SELECT IdPadre FROM Vaca WHERE Id = '$Vaca'";
+            $resultado = $con->query($query1);
+
+            if ($resultado->num_rows=0) {
+                $query1="UPDATE Vaca SET Nivel='1' WHERE Id = '$Vaca'";
+                $resultado = $con->query($query1);
+                $pa=2;
+            }
+            else
+            {
+                $Vaca=$resultado['IdPadre'];
+            }
+        }
+
 ?>
