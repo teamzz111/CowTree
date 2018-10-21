7b981baff2900fad19a051fcf6ea327a01cb401c
teamzz111 <aflr@outlook.com>
2018-10-20 20:40:40 -0500

    Cleaning unnecesary JS files

--- app/estructura/footer.php
+++ app/estructura/footer.php
@@ -5,8 +5,10 @@
     </div>
     <!-- End Wrapper -->
     <!-- All Jquery -->
+
     <script src="js/lib/jquery/jquery.min.js"></script>
     <!-- Bootstrap tether Core JavaScript -->
+     <script src="js/jquery.slimscroll.js"></script>
     <script src="js/lib/bootstrap/js/popper.min.js"></script>
     <script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
     <!-- slimscrollbar scrollbar JavaScript -->
@@ -14,29 +16,11 @@
     <!--Menu sidebar -->
     <script src="js/sidebarmenu.js"></script>
     <!--stickey kit -->
-    <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
-    <!--Custom JavaScript -->
 
-
+ <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
     <!-- Amchart -->
-    <script src="js/lib/morris-chart/raphael-min.js"></script>
 
-
-
-
-    <script src="js/lib/calendar-2/moment.latest.min.js"></script>
-    <!-- scripit init-->
-    <script src="js/lib/calendar-2/semantic.ui.min.js"></script>
-    <!-- scripit init-->
-    <script src="js/lib/calendar-2/prism.min.js"></script>
-    <!-- scripit init-->
-    <script src="js/lib/calendar-2/pignose.calendar.min.js"></script>
-    <!-- scripit init-->
-    <script src="js/lib/calendar-2/pignose.init.js"></script>
-
-    <script src="js/lib/owl-carousel/owl.carousel.min.js"></script>
-    <script src="js/lib/owl-carousel/owl.carousel-init.js"></script>
-    <script src="js/scripts.js"></script>
+   <script src="js/scripts.js"></script>
     <!-- scripit init-->
 
     <script src="js/custom.min.js"></script>
