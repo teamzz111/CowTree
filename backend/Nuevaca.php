0f9640271e3d26fa3c0fe07a119394605e49a7d7
Erika Infante <37392918+MonoAncestral@users.noreply.github.com>
2018-10-21 22:45:17 -0500

    Cow's attributes update

--- backend/Nuevaca.php
+++ backend/Nuevaca.php
@@ -7,8 +7,9 @@
     $Ejemplar = $_POST['ejemplar'];
 	$Estado=$_POST["estado"];
 	$Destino=$_POST["destino"];
-	$Edad=$_POST["edad"];
-	$Herrado=$_POST["herrado"];
+    $Edad=$_POST["edad"];
+    $Sexo=$_POST["sexo"];
+    $Herrado=$_POST["herrado"];
 	$Destetado=$_POST["destetado"];
 	$fecha_nacimiento=$_POST["fecha_nacimiento"];
 	$Encaste=$_POST["encaste"];
@@ -21,10 +22,8 @@
 	$Calificacion=$_POST["calificacion"];
 	$Comportamiento=$_POST["comportamiento"];
 	$Observadores=$_POST["observadores"];
-	$Pareja=$_POST["idpadre"];
-	$Padre=$_POST["idpareja"];
-
-
+    $Padre=$_POST["idpadre"];
+    $Madre=$_POST["idmadre"];
 
     $con = new mysqli($host, $user, $pass, $db);
     $con->query("SET NAMES 'utf8'");
@@ -40,17 +39,43 @@
     }
     else{
         $query = "INSERT INTO vaca VALUES (
-        '$Ejemplar','$Nombre' ,'$Estado','$Destino','$Edad','$Herrado','$Destetado','$fecha_nacimiento','$Encaste',
-        '$Reseña','$Arbol_id','$Ganaderia','$Criador','$Fenotipo','$Defector','$Calificacion','$Comportamiento',
-        '$Observadores','$Pareja','$Padre')";
+        '$Ejemplar','$Nombre' ,'$Estado','$Destino','$Edad','$Sexo','$Herrado','$Destetado','$fecha_nacimiento','$Encaste',
+        '$Reseña','','$Ganaderia','$Criador','$Fenotipo','$Defector','$Calificacion','$Comportamiento',
+        '$Observadores','$Padre',$Madre)";
         $rs = $con->query($query);
-        if ($rs) {
-            echo 'true';
-        }
-        else {
-            echo 'false';
+        if ($rs) { echo 'true';}
+        else{echo 'false';}
+        
+        $arbol="Arbol de $Nombre"
+        if($Padre=="")
+        {
+            $query= "INSERT INTO arbol VALUES (0, '$arbol')";
+            $rs=$con->query($query);
+            if ($rs) // si la vaca que se acaba de crear no tiene padre, se crea un árbol y se le pone a la misma el id de ese arbol nuevo
+            {
+                $query="SELECT Id FROM arbol ORDER BY ID DESC LIMIT 1"; //BUSCA EL ÚLTIMO ÁRBOL QUE FUE CREADO PARA METER LAS VACAS AHÍ
+                $result= $con->query($query); 
+                if (!$result) {
+                    trigger_error('Invalid query: ' . $con->error);
+                }
+                else 
+                {
+                    echo $query;
+                    $row = $result ->fetch_array(MYSQLI_ASSOC);
+                    $Tree= $row['Id'];//el id del árbol que se acaba de crear
+    
+                    $query1= "UPDATE vaca SET Arbol_Id= '$Tree' WHERE Ejemplar='$Ejemplar'"; //introduce la vaca al árbol recien creado
+                    $rs = $con->query($query1);
+                    if ($rs) { echo 'true';}
+                    else{echo 'false';} 
+                }  
             }
+            else{echo 'false';}
         }
+        else 
+        }
+
+    
         
     
 ?>
