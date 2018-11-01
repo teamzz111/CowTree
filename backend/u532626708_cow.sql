c2aa8bbd02b13eafefd845004bbaa9fdafcc2aef
Erika Infante <erikainfantedoo@gmail.com>
2018-11-01 07:41:21 -0500

    Db

--- backend/u532626708_cow.sql
+++ backend/u532626708_cow.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-10-2018 a las 00:23:34
+-- Tiempo de generación: 01-11-2018 a las 13:39:58
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 7.2.11
 
@@ -38,22 +38,20 @@ CREATE TABLE `arbol` (
 --
 
 INSERT INTO `arbol` (`Id`, `Nombre`) VALUES
-(33, 'Arbol de Vaca'),
-(34, 'Arbol de Vaca'),
-(35, 'Arbol de ,mm,nmn'),
-(36, 'Arbol de ,mm,nmn'),
-(37, 'Arbol de ,mm,nmn'),
-(38, 'Arbol de '),
-(39, 'Arbol de asfasf'),
-(40, 'Arbol de asd'),
-(41, 'Arbol de safsaf'),
-(42, 'Arbol de safsaf'),
-(43, 'Arbol de safsaf'),
-(44, 'Arbol de safsaf'),
-(45, 'Arbol de safsaf'),
-(46, 'Arbol de safsaf2'),
-(47, 'Arbol de safsaf2'),
-(48, 'Arbol de safsaf2');
+(96, 'Arbol de Junior'),
+(95, 'Arbol de Lo que sea'),
+(94, 'Arbol de Cj'),
+(93, 'Arbol de Jesus de Nazaret'),
+(92, 'Arbol de Harry Potter'),
+(91, 'Arbol de Larry'),
+(90, 'Arbol de Dios destructor'),
+(89, 'Arbol de Charlie'),
+(88, 'Arbol de Lord Tocino'),
+(87, 'Arbol de Hamburguesito'),
+(86, 'Arbol de Hamburguesito'),
+(85, 'Arbol de Hamburguesito'),
+(84, 'Arbol de Hamburguesito'),
+(83, 'Arbol de Hambu');
 
 -- --------------------------------------------------------
 
@@ -104,6 +102,48 @@ CREATE TABLE `producto` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `rama`
+--
+
+CREATE TABLE `rama` (
+  `IdArbol` int(3) NOT NULL,
+  `IdVaca` varchar(100) COLLATE latin1_spanish_ci NOT NULL,
+  `Nivel` int(3) DEFAULT NULL,
+  `posicion` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `rama`
+--
+
+INSERT INTO `rama` (`IdArbol`, `IdVaca`, `Nivel`, `posicion`) VALUES
+(87, '1', 1, 1),
+(88, '2', 1, 1),
+(87, '2', 2, 2),
+(89, '3', 1, 1),
+(87, '3', 2, 3),
+(90, '4', 1, 1),
+(87, '4', 2, 1),
+(91, '5', 1, 1),
+(88, '5', 2, 0),
+(87, '5', 3, 1),
+(92, '6', 1, 1),
+(88, '6', 2, 0),
+(87, '6', 3, 2),
+(93, '7', 1, 1),
+(89, '7', 2, 0),
+(87, '7', 3, 3),
+(94, '8', 1, 1),
+(90, '8', 2, 0),
+(87, '8', 3, 4),
+(96, '9', 1, 1),
+(91, '9', 2, 0),
+(88, '9', 3, 0),
+(87, '9', 4, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
@@ -134,17 +174,14 @@ CREATE TABLE `vaca` (
   `Ejemplar` varchar(100) NOT NULL,
   `Nombre` varchar(45) DEFAULT NULL,
   `Estado` varchar(45) NOT NULL,
-  `Nivel` int(3) DEFAULT NULL,
   `Destino` varchar(45) NOT NULL,
-  `Edad` varchar(100) NOT NULL,
+  `Edad` tinyint(2) NOT NULL,
   `Sexo` varchar(10) NOT NULL,
   `Herrado` varchar(45) DEFAULT NULL,
   `Destetado` varchar(45) DEFAULT NULL,
   `Fecha_nacimiento` date DEFAULT NULL,
   `Encaste` varchar(45) NOT NULL,
   `Reseña` varchar(45) NOT NULL,
-  `Arbol_IdP` int(11) DEFAULT NULL,
-  `Arbol_IdM` int(11) DEFAULT NULL,
   `Ganaderia_Id` int(11) NOT NULL,
   `Criador_Id` int(11) NOT NULL,
   `Fenotipo` varchar(45) DEFAULT NULL,
@@ -160,13 +197,17 @@ CREATE TABLE `vaca` (
 -- Volcado de datos para la tabla `vaca`
 --
 
-INSERT INTO `vaca` (`Ejemplar`, `Nombre`, `Estado`, `Nivel`, `Destino`, `Edad`, `Sexo`, `Herrado`, `Destetado`, `Fecha_nacimiento`, `Encaste`, `Reseña`, `Arbol_IdP`, `Arbol_IdM`, `Ganaderia_Id`, `Criador_Id`, `Fenotipo`, `Defectos`, `Calificacion`, `Comportamiento`, `Observadores`, `IdPadre`, `IdMadre`) VALUES
-('ASP-188-188-2014-H-SOSPECHOSA', 'Prueba 1', 'Vivo', NULL, 'Tentar', '4 años, seis mese y 1 día', '', 'Sí', 'No', '2018-09-14', 'Baltasar Iban', 'En buen estado', 0, NULL, 0, 0, 'Test', 'Ninguni', '5', 'Normal', 'Andrés Largo', '0', NULL),
-('asf', 'safsaf', 'dsf', 0, 'wer', '423', 'sí', 'sí', 'sí', '3432-02-04', 'sdf', 'sdf', 0, 0, 0, 0, 'wre', 'sdf', '324', 'sdfdsf', 'sdfsdf', '', ''),
-('asfsdf', 'safsaf', 'dsf', 0, 'wer', '423', 'sí', 'sí', 'sí', '3432-02-04', 'sdf', 'sdf', 0, 0, 0, 0, 'wre', 'sdf', '324', 'sdfdsf', 'sdfsdf', '', ''),
-('asfsdfqwe', 'safsaf2', 'dsf', 0, 'wer', '423', 'sí', 'sí', 'sí', '3432-02-04', 'sdf', 'sdf', 46, 46, 0, 0, 'wre', 'sdf', '324', 'sdfdsf', 'sdfsdf', '', ''),
-('asfsdfqwea', 'safsaf2', 'dsf', 0, 'wer', '423', 'sí', 'sí', 'sí', '3432-02-04', 'sdf', 'sdf', 47, 47, 0, 0, 'wre', 'sdf', '324', 'sdfdsf', 'sdfsdf', '', ''),
-('asfsdfqweaa', 'safsaf2', 'dsf', 0, 'wer', '423', 'sí', 'sí', 'sí', '3432-02-04', 'sdf', 'sdf', 48, 48, 0, 0, 'wre', 'sdf', '324', 'sdfdsf', 'sdfsdf', '', '');
+INSERT INTO `vaca` (`Ejemplar`, `Nombre`, `Estado`, `Destino`, `Edad`, `Sexo`, `Herrado`, `Destetado`, `Fecha_nacimiento`, `Encaste`, `Reseña`, `Ganaderia_Id`, `Criador_Id`, `Fenotipo`, `Defectos`, `Calificacion`, `Comportamiento`, `Observadores`, `IdPadre`, `IdMadre`) VALUES
+('ASP-188-188-2014-H-SOSPECHOSA', 'Prueba 1', 'Vivo', 'Tentar', 4, '', 'Sí', 'No', '2018-09-14', 'Baltasar Iban', 'En buen estado', 0, 0, 'Test', 'Ninguni', '5', 'Normal', 'Andrés Largo', '0', NULL),
+('4', 'Dios destructor', 'Vivo', 'Reinar el mundo', 6, 'sí', 'sí', 'sí', '4211-12-12', 'El mejor papu', '', 5, 0, 'No jé', 'Ninguno, es perfecto', '1000', 'Excelente', 'El universo entero', '1', ''),
+('1', 'Hamburguesito', 'Vivo', 'Burguerking', 15, 'sí', 'sí', 'sí', '2111-02-11', 'NEL', 'Delicioso', 5, 0, 'Shido', 'no sirve para nada', '10', 'prron', 'Chewaca', '', ''),
+('2', 'Lord Tocino', 'Deprimido', 'asda', 13, 'sí', 'sí', 'sí', '2027-11-15', '', 'w423', 5, 0, 'algo', 'Se va a morir', '45', 'Terrible', 'Diosito', '1', ''),
+('3', 'Charlie', 'Activo', 'La muerte', 8, 'sí', 'sí', 'sí', '2019-02-03', '32', 'w423', 5, 0, 'khe', 'no sirve para nada', '10', 'increíble', 'Kratos', '1', ''),
+('5', 'Larry', 'Activo', 'Burguerking', 12, 'sí', 'sí', 'sí', '4520-02-11', 'pos si', 'Not bad', 5, 0, 'ni idea', 'no hay ', '99', 'maravilloso', 'su mami', '2', ''),
+('6', 'Harry Potter', 'Hechizado', 'Hogwarts', 20, 'sí', 'sí', 'sí', '2405-12-10', 'No tiene', 'Es un buen mago', 5, 0, 'no tiene nada ', 'Tiene una cicatriz en la frente ', '50', 'Rebelde', 'nadie, usa una capa invisible', '2', ''),
+('7', 'Jesus de Nazaret', 'vivo en todos nosotros', 'El cielo', 33, 'sí', 'sí', 'sí', '0001-01-01', 'nel', 'Bueno', 5, 0, 'no tiene nada ', 'Ninguno', '80', 'Bueno', 'Diosito', '3', ''),
+('8', 'Cj', 'Negro', 'las venturas', 24, 'sí', 'sí', 'sí', '2004-02-10', 'nada', 'prron', 5, 0, 'ni idea', 'Es negro', '12', 'Como todo un gangster', 'Ryder', '4', ''),
+('9', 'Junior', 'Vivo', 'vivir feliz', 45, 'sí', 'sí', 'sí', '0042-02-11', '2', '21212', 5, 0, 'asd', 'hk', '21', 'Chingon', '12', '5', '');
 
 --
 -- Índices para tablas volcadas
@@ -203,7 +244,6 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `vaca`
   ADD PRIMARY KEY (`Ejemplar`),
-  ADD KEY `fk_Vaca_Arbol_idx` (`Arbol_IdP`),
   ADD KEY `fk_Vaca_Ganaderia1_idx` (`Ganaderia_Id`),
   ADD KEY `fk_Vaca_Criador1_idx` (`Criador_Id`);
 
@@ -215,7 +255,7 @@ ALTER TABLE `vaca`
 -- AUTO_INCREMENT de la tabla `arbol`
 --
 ALTER TABLE `arbol`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
 
 --
 -- AUTO_INCREMENT de la tabla `ganaderia`
